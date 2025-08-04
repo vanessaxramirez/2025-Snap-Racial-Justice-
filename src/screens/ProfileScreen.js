bed72251cd679b2d1ffbd27c0de0305e3898366a
Vanessa Ramirez <130528072+vanessaxramirez@users.noreply.github.com>
2025-08-04 09:03:20 -0700

    fixed astrology and communities button

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,4 +1,12 @@
-import { Image, Text, View, Button, StyleSheet, Pressable } from "react-native";
+import {
+  Image,
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 import { supabase } from "../utils/hooks/supabase";
 import { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
@@ -45,7 +53,7 @@ export default function ProfileScreen() {
           user.user_metadata &&
           user.user_metadata.email.slice(
             0,
-            user.user_metadata.email.indexOf("@"), // gets part before @ of email address, should use profile username instead
+            user.user_metadata.email.indexOf("@") // gets part before @ of email address, should use profile username instead
           )}
       </Text>
       <Button
@@ -56,6 +64,32 @@ export default function ProfileScreen() {
         color="#841584"
         accessibilityLabel="Learn more about this purple button"
       />
+      <View style={styles.touchButton}>
+        <TouchableOpacity
+          onPress={() => console.log("Bubble button pressed")}
+          style={{
+            height: "100",
+            width: "95%",
+            backgroundColor: "#f0f0f0",
+            paddingVertical: 10,
+            paddingHorizontal: 20,
+            borderRadius: 10,
+            alignSelf: "center", 
+            marginVertical: 10,
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.2,
+            shadowRadius: 4,
+          }}
+        >
+          <Text style={{ color: "black", fontSize: "30", padding: "5" }}>
+            Growth Circles
+          </Text>
+          <Text style={{ color: "#333", fontSize: "15", padding: "5" }}>
+            251 Members
+          </Text>
+        </TouchableOpacity>
+      </View>
       <Button onPress={handleSignOut} title="Log Out" />
       <Pressable>
         <Button
@@ -80,5 +114,11 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 150 / 2,
     alignItems: "center",
+  },
+  touchButton: {
+    width: "100%",
+    height: "150",
+    alignItems: "center",
+    borderRadius: "5",
   },
 });
